2d3fcda6c77847228c4c74c5bfc49fe2bf84e6fb
xuliang <xuliang@kuaizi.ai>
2022-10-13 16:52:38 +0800

    style: 初始化 core

---
+++ packages/core/src/index.ts
@@ -0,0 +1,26 @@
+interface PluginCtor {
+  selfPluginName: string
+  new(auxiliary: any): any
+}
+
+interface PluginItem {
+  name: string
+  ctor: PluginCtor
+}
+
+export class AuxiliaryTool {
+  static plugins: PluginItem[] = []
+  use(ctor: PluginCtor) {
+    const name = ctor.selfPluginName
+    const installed = AuxiliaryTool.plugins.some(plugin => ctor === plugin.ctor)
+    if (installed) return AuxiliaryTool
+    if (!name) {
+      console.log(`Plugin Class must specify plugin's name in static property by 'selfPluginName' field.`)
+    }
+    AuxiliaryTool.plugins.push({
+      name: ctor.selfPluginName,
+      ctor
+    })
+    return AuxiliaryTool
+  }
+}
